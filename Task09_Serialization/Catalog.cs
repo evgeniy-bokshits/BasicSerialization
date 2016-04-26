f372e53ed6a02a49f8811fc70fdeb01583bfc6e1
evgeniy.bokshits <evgeniy.bokshits@gmail.com>
2016-04-26 17:16:01 +0300

    recovery
    added after review

---
+++ Task09_Serialization/Catalog.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Task09_Serialization
+{
+    class Catalog
+    {
+        [XmlElement("Book")]
+        public List<Book> Books { get; set; }
+    }
+
+    class Book
+    {
+        [XmlElement("author")]
+        public string Author { get; set; }
+
+        [XmlElement("title")]
+        public string Title { get; set; }
+
+        [XmlElement("genre")]
+        public Genre Genre { get; set; }
+
+        [XmlElement("publisher")]
+        public string Publisher { get; set; }
+
+        [XmlElement("publish_date")]
+        public DateTime PublishDate { get; set; }
+
+        [XmlElement("description")]
+        public string Description { get; set; }
+
+        [XmlElement("registration_date")]
+        public DateTime RegistrationDate { get; set; }
+    }
+
+    enum Genre
+    {
+        Computer,
+        Fantasy,
+        Romance,
+        Horror,
+        ScienceFiction
+    }
+}
---
+++ Task09_Serialization/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace Task09_Serialization
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Serialization();
+            Deserialization();
+            Console.ReadLine();
+        }
+
+
+        private static void Serialization()
+        {
+            using (Stream file = new FileStream(@"books2.xml", FileMode.OpenOrCreate))
+            {
+                var serializer = new XmlSerializer(typeof(Catalog));
+                //to remove redundant <Books> tag
+                XmlSerializerNamespaces ns = new XmlSerializerNamespaces();
+                ns.Add("", "");
+                var catalog = new Catalog
+                {
+                    Books = new List<Book>
+                    {
+                        new Book
+                        {
+                            Author = "Mikita",
+                            Description = "Awesome book",
+                            Genre = Genre.Fantasy,
+                            PublishDate = DateTime.Today.Date,
+                            Publisher = "Tantik",
+                            RegistrationDate = DateTime.Today - new TimeSpan(1, 0, 0, 0),
+                            Title = "Abra-kadabra"
+                        },
+                        new Book
+                        {
+                            Author = "Zlata",
+                            Description = "molecular biology",
+                            Genre = Genre.Horror,
+                            PublishDate = DateTime.Today.Date,
+                            Publisher = "Ataraxia",
+                            RegistrationDate = DateTime.Today - new TimeSpan(1, 0, 0, 0),
+                            Title = "Abra-kadabra"
+                        }
+                    }
+                };
+
+                serializer.Serialize(file, catalog, ns);
+            }
+        }
+
+        private static void Deserialization()
+        {
+            using (Stream file = new FileStream(@"books2.xml", FileMode.Open))
+            {
+                var serializer = new XmlSerializer(typeof (Catalog));
+
+                var catalogs = serializer.Deserialize(file) as Catalog;
+
+                if (catalogs != null)
+                {
+                    foreach (var book in catalogs.Books)
+                    {
+                        Console.WriteLine(book.Author);
+                        Console.WriteLine(book.Title);
+                        Console.WriteLine(book.PublishDate);
+                        Console.WriteLine();
+                    }
+                }
+            }
+        }
+
+    }
+}
